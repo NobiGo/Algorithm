9a2ba96c5decbef0d40a754e57f74eaed2789f94
NobiGo <nobigogle@gmail.com>
2017-03-05 23:10:28 +0800

    调试——输入输出Jar

---
+++ src/dx/com/one/TestAlgorithm.java
@@ -0,0 +1,20 @@
+package dx.com.one;
+
+
+import org.junit.Test;
+
+/**
+ * Created by dx on 2017/3/5.
+ */
+public class TestAlgorithm {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    @Test
+    public void helloTest(){
+
+    }
+}
